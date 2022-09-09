a9273061edde83839a5ca772bd30740f49a7f4fc
Sniperspyder2550 <86437461+Sniperspyder2550@users.noreply.github.com>
2022-09-09 21:34:35 +0200

    Update Zinssatz.java
    actual version of the program, messed it up

--- src/blatt01/Zinssatz.java
+++ src/blatt01/Zinssatz.java
@@ -1,2 +1,18 @@
-package blatt01;public class Zinssatz {
+package blatt01;
+
+public class Zinssatz {
+    public static void main(String[] args) {
+        double startKapital = 1220.00;
+        double zinssatz = 2.25;
+        double zinsen = 0.00;
+        double neuKapital = 0.00;
+
+        zinsen = (startKapital * zinssatz) / 100;
+        neuKapital = startKapital + zinsen;
+
+        System.out.println("Altes Kapital: " + startKapital + "€");
+        System.out.println("Zinssatz: " + zinssatz + "%\n");
+        System.out.println("Zinsen: " + zinsen + "€");
+        System.out.println("Neues Kapital: " + neuKapital + "€");
+    }
 }
