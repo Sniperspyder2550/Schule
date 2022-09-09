0ac7610be6b1b6e1fc4019d06fb3183af2551417
Sniperspyder2550 <86437461+Sniperspyder2550@users.noreply.github.com>
2022-09-09 22:06:35 +0200

    Update TypKonversation.java

--- src/blatt01/TypKonversation.java
+++ src/blatt01/TypKonversation.java
@@ -1,2 +1,24 @@
-package blatt01;public class TypKonversation {
+package zusatz;
+
+public class TypKonversation {
+
+    public static void main(String[] args) {
+        int a = 14;
+        double b = -21.25;
+
+        int c = (int) b;
+        double d = (double) a;
+
+        System.out.println(c);
+        System.out.println(d);
+
+        char e = 'a';
+        int f = 65;
+
+        int g = (int) e;
+        char h = (char) f;
+
+        System.out.println(g);
+        System.out.println(h);
+    }
 }
