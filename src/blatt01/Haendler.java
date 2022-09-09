bb5479a133e8328f1c4ecf5a508e464545b8fc8e
Sniperspyder2550 <86437461+Sniperspyder2550@users.noreply.github.com>
2022-09-09 21:49:21 +0200

    Update Haendler.java
    Actual file, I messed up earlier

--- src/blatt01/Haendler.java
+++ src/blatt01/Haendler.java
@@ -1,2 +1,23 @@
-package blatt01;public class Haendler {
+package blatt01;
+
+public class haendler {
+
+    public static void main(String[] args) {
+        int mengeSD = 7;
+        int mengeMini = 4;
+        int mengeMicro = 12;
+        double preisSD = 5.00;
+        double preisMini = 8.00;
+        double preisMicro = 12.00;
+
+        double preis = (mengeSD * preisSD) + (mengeMini * preisMini) + (mengeMicro * preisMicro);
+
+        System.out.println("----------------------------");
+        System.out.println("\t\tWarenkorb\n");
+        System.out.println(mengeSD + " normale SD-Karten");
+        System.out.println(mengeMini + " MiniSD-Karten");
+        System.out.println(mengeMicro + " MicroSD-Karten");
+        System.out.println("\nGesamtpreis: "+ preis + "€");
+        System.out.println("----------------------------");
+    }
 }
