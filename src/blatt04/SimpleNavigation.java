1f0e9cc9d0298c66acf3e0ea0d527b77dfd58fdc
Sniperspyder2550 <86437461+Sniperspyder2550@users.noreply.github.com>
2022-11-06 21:26:19 +0100

    Update SimpleNavigation.java

--- src/blatt04/SimpleNavigation.java
+++ src/blatt04/SimpleNavigation.java
@@ -20,20 +20,23 @@ public class SimpleNavigation {
         int p2y = sc.nextInt();
 
 
-        //Solange Punkt1 nicht auf Punkt 2 liegt, werden die
+        //Solange Punkt1 nicht auf Punkt 2 liegt, werden die Schritte ausgeführt.
         if (p1x != p2x && p1y != p2y) {
             while (p1x != p2x) {
+                //Hier werden die Punkte auf die jeweilig gleiche X Koordinate geschoben
                 if (p1x > p2x) {
                     System.out.println(" Links(" + p1x + "|" + p1y + ")");
                     p1x--;
+                    //Hier werden die Punkte auf die jeweilig gleiche X Koordinate geschoben
                 } else {
                     System.out.println(" Rechts(" + p1x + "|" + p1y + ")");
                     p1x++;
-
+                    //Hier werden die Punkte auf die jeweilig gleiche Y Koordinate geschoben
                     while (p1y != p2y) {
                         if (p1y > p2y) {
                             System.out.println(" Runter(" + p1x + "|" + p1y + ")");
                             p1y--;
+                            //Hier werden die Punkte auf die jeweilig gleiche Y Koordinate geschoben
                         } else {
                             System.out.println(" Hoch(" + p1x + "|" + p1y + ")");
                             p1y++;
@@ -42,6 +45,7 @@ public class SimpleNavigation {
                     }
                 }
             }
+            //Ausgabe
         } else {
             System.out.println("Der Punkt hat das Ziel (" + p1x + "|" + p1y + ") erreicht.");
         }
